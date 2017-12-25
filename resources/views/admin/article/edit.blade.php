acc1291b6c52b3e20fed761c879c1a18e7522c93
ouhao <ouhao@ouxiaohao.com>
2017-12-25 23:43:08 +0800

    edit arc md

--- resources/views/admin/article/edit.blade.php
+++ resources/views/admin/article/edit.blade.php
@@ -18,18 +18,10 @@
             <div class="form-group">
                 <label class="col-sm-2 control-label">文章内容</label>
                 <div class="col-sm-10">
-                    <!-- 加载编辑器的容器 -->
-                    <script id="container" name="Article[content]" type="text/plain">
-                        {!! old('Article')['content'] ? old('Article')['content'] : $article->content !!}
-                    </script>
-                    <!-- 配置文件 -->
-                    <script type="text/javascript" src="{{ asset('org/ueditor/ueditor.config.js') }}"></script>
-                    <!-- 编辑器源码文件 -->
-                    <script type="text/javascript" src="{{ asset('org/ueditor/ueditor.all.js') }}"></script>
-                    <!-- 实例化编辑器 -->
-                    <script type="text/javascript">
-                        var ue = UE.getEditor('container');
-                    </script>
+                    @include('editor::head')
+                    <div class="editor">
+                        <textarea id='myEditor' name="Article[content]">{!! old('Article')['content'] ? old('Article')['content'] : $article->content !!}</textarea>
+                    </div>
                 </div>
             </div>
             <div class="form-group">
