55ac8c972d23309248b279d24eaff2bb564d932d
ouhao <ouhao@ouxiaohao.com>
2018-03-12 00:04:16 +0800

    test

--- resources/views/home/partials/footer.blade.php
+++ resources/views/home/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <p>
         <span>© 2017 wshiyi.com版权所有 </span>
-        ICP证：<a href="http://www.miitbeian.gov.cn" target="_blank" >111豫ICP备15029544号-2</a>
+        ICP证：<a href="http://www.miitbeian.gov.cn" target="_blank" >豫ICP备15029544号-2</a>
     </p>
     {{--<p>联系邮箱：ouhao@ouxiaohao.com</p>--}}
 </footer>
