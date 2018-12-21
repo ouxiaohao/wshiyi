6654b95a3a5fa4d4f074b52b4d0c50a9abe5b091
ouhao <ouhao@ouxiaohao.com>
2018-12-21 22:41:04 +0800

    header

--- resources/views/home/partials/header.blade.php
+++ resources/views/home/partials/header.blade.php
@@ -4,14 +4,7 @@
             <nav class="navbar navbar-default" role="navigation">
                 <div class="container-fluid">
                     <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse"
-                            data-target="#example-navbar-collapse">
-                            <span class="sr-only">切换导航</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="{{ url('/') }}">OuHao</a>
+                        <a class="navbar-brand" href="{{ url('/') }}">Wshiyi</a>
                     </div>
                     <div class="collapse navbar-collapse" id="example-navbar-collapse">
                         <ul class="nav navbar-nav">
