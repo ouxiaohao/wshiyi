a73ff5373be64dde5ca16fcb00af4bb3174199f5
ouhao <ouhao@ouxiaohao.com>
2018-12-23 09:20:26 +0800

    page

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -73,7 +73,7 @@ class IndexController extends Controller
                 'category.title as category_title','category.name as category_name'
                 )
             ->distinct()
-            ->paginate(2);
+            ->paginate(8);
 //        追加文章的标签
         foreach ($articles as $article) {
             $article->tags = ArticleTag::join('tag','article_tag.tag_id','=','tag.id')
